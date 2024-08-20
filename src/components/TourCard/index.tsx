4537d23b1c4490470e30db5b9d66aaf89ba53d79
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-08-20 13:06:12 -0300

    feat: add TourCard component

---
+++ src/components/TourCard/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Stars from '../Stars';
+import { TourCardProps } from './types';
+import { FaRegClock } from 'react-icons/fa6';
+import { FaRegHeart } from 'react-icons/fa';
+
+const TourCard = ({ imgUrl, locale, title, stars, reviews, days, minPrice }: TourCardProps) => {
+  return (
+    <a 
+      href='#'
+      className='w-60 bg-gray-100 flex flex-col items-center relative'>
+      <button className='absolute bg-white rounded-full p-2 top-4 right-4 hover:text-red-600'>
+          <FaRegHeart size={12} />
+      </button>
+      <img src={imgUrl} className='h-50' alt="Tour" />
+      <div className='w-[95%] px-2 flex flex-col justify-center gap-1'>
+        <p className='text-gray-500 pt-2'>{locale}</p>
+        <h3 className='font-bold text-h6'>{title}</h3>
+        <p className='flex items-center justify-between text-gray-500 py-1'>
+          <span className='flex gap-1'>
+            <Stars value={stars} />
+            <span>{reviews} reviews</span>
+          </span>
+          <span className='flex items-center gap-1'>
+            <FaRegClock />
+            {days} days
+          </span>
+        </p>
+        <p className='flex justify-between text-gray-500 text-body border-t-2 py-2'>
+          Starting from 
+          <span className='text-black text-h6 font-price italic font-semibold'>${minPrice}</span>
+        </p>
+      </div>
+    </a>
+  );
+};
+
+export default TourCard;
