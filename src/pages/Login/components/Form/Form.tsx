6ac7d5dc341959222a3ba5006edc94a8de8250e1
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-08-20 15:34:54 -0300

    fix: removing main from Form component

--- src/pages/Login/components/Form/Form.tsx
+++ src/pages/Login/components/Form/Form.tsx
@@ -9,26 +9,24 @@ import { FcGoogle } from "react-icons/fc";
 import { FormProps } from './types';
 
 const Form = ({ title, message, handleSubmit,buttonText, changeMessage, handleClick, changeClickMessage }: FormProps) => {
-  return <main className='w-full h-full flex flex-col items-center justify-center'>
-    <form className='w-[35%] min-h-[80%] p-10 bg-surface border border-black rounded flex flex-col justify-between'>
-      <Titles colorTitle='black' subtitle='Trisog' title={title}/>
-      <h2 className='text-center text-h6 font-bold'>{message}</h2>
-      <InputField Icon={AiOutlineMail} onChange={() => console.log('')} placeholder='Email...'/>
-      <InputField Icon={FaLock} onChange={() => console.log('')} placeholder='Password...'/>
-      <SubmitButton onClick={handleSubmit} text={buttonText}/>
-      <div className='flex gap-6'>
-        <IconButton Icon={FcGoogle} handleClick={() => console.log('google')}/>
-        <IconButton Icon={FaFacebook}  handleClick={() => console.log('facebook')}/>
-      </div>
-      <p className='flex justify-center gap-4 font-semibold'>{changeMessage}
-        <span className='text-brand_2 hover:cursor-pointer hover:text-red-600 font-price font-bold'
-          onClick={handleClick}
-        >
-          {changeClickMessage}
-        </span>
-      </p>
-    </form>
-  </main>
+  return <form className='w-[35%] min-h-[80%] p-10 bg-surface border border-black rounded flex flex-col justify-between'>
+    <Titles colorTitle='black' subtitle='Trisog' title={title}/>
+    <h2 className='text-center text-h6 font-bold'>{message}</h2>
+    <InputField Icon={AiOutlineMail} onChange={() => console.log('')} placeholder='Email...'/>
+    <InputField Icon={FaLock} onChange={() => console.log('')} placeholder='Password...'/>
+    <SubmitButton onClick={handleSubmit} text={buttonText}/>
+    <div className='flex gap-6'>
+      <IconButton Icon={FcGoogle} handleClick={() => console.log('google')}/>
+      <IconButton Icon={FaFacebook}  handleClick={() => console.log('facebook')}/>
+    </div>
+    <p className='flex justify-center gap-4 font-semibold'>{changeMessage}
+      <span className='text-brand_2 hover:cursor-pointer hover:text-red-600 font-price font-bold'
+        onClick={handleClick}
+      >
+        {changeClickMessage}
+      </span>
+    </p>
+  </form>
 } 
 
 export default Form;
