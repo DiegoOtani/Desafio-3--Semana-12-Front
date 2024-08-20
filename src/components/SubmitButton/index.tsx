9ce75104fb54ea45d911adfb91209f0f2aa18edd
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-08-20 15:24:40 -0300

    feat: add SubmitButton component

---
+++ src/components/SubmitButton/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { SubmitButtonProps } from './types';
+
+const SubmitButton = ({ onClick, text }: SubmitButtonProps) => {
+  return <button 
+      type='button'
+      className='bg-brand_2 text-white font-semibold rounded p-2 hover:bg-red-500 border'
+      onClick={onClick}
+    >
+    {text}
+  </button>
+} 
+
+export default SubmitButton;
