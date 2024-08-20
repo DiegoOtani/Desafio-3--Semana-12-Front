f48a0125ad0df6932d4a2fcaf1151edd8f9e1a04
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-08-20 16:00:55 -0300

    feat: add state for change login and register form

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Form from './components/Form/Form';
 
 const Login = () => {
+  const [ haveLogin, setHaveLogin ] = useState<boolean>(true);
+
   return <main className='w-full h-full flex flex-col items-center justify-center'>
-    <Form 
+    {haveLogin ? (
+      <Form 
       title='Sign in'
       message='Enter your details to continue your journey'
       handleSubmit={() => console.log('submit')}
       buttonText='Login'
       changeMessage="Don't have login yet?"
       changeClickMessage="Sign up"
-      handleClick={() => console.log('change form')}
+      handleClick={() => setHaveLogin(!haveLogin)}
     />
+    ) : (
+      <Form 
+        title='Sign up'
+        message='Enter your details to start our journey'
+        handleSubmit={() => console.log('submit')}
+        buttonText='Register'
+        changeMessage='Already have a login?'
+        changeClickMessage='Sign in'
+        handleClick={() => setHaveLogin(!haveLogin)}
+      />
+    )}
+    
   </main>
 }
 
