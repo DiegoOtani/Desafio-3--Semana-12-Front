2224eccfe6780e679bc34bde5c6ba9dd42316928
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-08-20 15:22:10 -0300

    refactor: changing the props use

--- src/components/Titles/index.tsx
+++ src/components/Titles/index.tsx
@@ -4,13 +4,13 @@ import { TitlesProps } from './types';
 const Titles = ({ title, subtitle, colorTitle }: TitlesProps) => {
   return (
     <div className='text-center'>
-      <h2 className='text-brand_2 text-h6 font-semibold'>{title}</h2>
+      <h2 className='text-brand_2 text-h6 font-semibold font-price italic'>{subtitle}</h2>
       <h1 className={`
         ${colorTitle === 'black'
           ? 'text-black' 
           : 'text-white'} 
           font-bold text-h4
-        `}>{subtitle}</h1>
+        `}>{title}</h1>
     </div>
   )
 }
