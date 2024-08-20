a73017855616dd4c4b52b969c77d0036e824ddef
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-08-20 13:07:33 -0300

    feat(TourCard): add favorite action in card

--- src/components/TourCard/index.tsx
+++ src/components/TourCard/index.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Stars from '../Stars';
 import { TourCardProps } from './types';
 import { FaRegClock } from 'react-icons/fa6';
-import { FaRegHeart } from 'react-icons/fa';
+import { FaRegHeart, FaHeart } from 'react-icons/fa';
 
 const TourCard = ({ imgUrl, locale, title, stars, reviews, days, minPrice }: TourCardProps) => {
+  const [isHeartFilled, setIsHeartFilled] = useState(false);
+
+  const handleHeartClick = () => {
+    setIsHeartFilled(prevState => !prevState);
+  };
+
   return (
     <a 
       href='#'
       className='w-60 bg-gray-100 flex flex-col items-center relative'>
-      <button className='absolute bg-white rounded-full p-2 top-4 right-4 hover:text-red-600'>
+      <button
+        className='absolute bg-white rounded-full p-2 top-4 right-4 hover:text-red-600'
+        onClick={handleHeartClick}
+      >
+        {isHeartFilled ? (
+          <FaHeart size={12} color='red' />
+        ) : (
           <FaRegHeart size={12} />
+        )}
       </button>
       <img src={imgUrl} className='h-50' alt="Tour" />
       <div className='w-[95%] px-2 flex flex-col justify-center gap-1'>
