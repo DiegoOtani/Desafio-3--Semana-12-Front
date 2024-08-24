a20813596e8ecc7152694c13590580c8af4122de
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-08-24 00:00:47 -0300

    feat: add outlet in app and pages includes in router

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,28 @@ import App from './App.tsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Login from './pages/Login/index.tsx'
+import Home from './pages/Home/index.tsx'
+import Tour from './pages/Tour/index.tsx'
+import TourDetails from './pages/TourDetails/index.tsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element:<App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/tour',
+        element: <Tour />
+      },
+      {
+        path: '/tour/:details',
+        element: <TourDetails />
+      },
+    ]
   },
   {
     path: '/login',
