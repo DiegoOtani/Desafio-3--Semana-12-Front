936752b3f92f5c3cefa41c9d1dac05fe492f0a8e
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-08-23 23:50:14 -0300

    feat: initial router structure

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Login from './pages/Login/index.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element:<App />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
