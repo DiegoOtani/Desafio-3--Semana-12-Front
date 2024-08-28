2eff29edcd07c58c81871db4651d79e9652697a6
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-08-28 14:49:18 -0300

    fix: adjust to prevous and next no set more values than exists

--- src/pages/Tour/Components/Pagination/index.tsx
+++ src/pages/Tour/Components/Pagination/index.tsx
@@ -3,22 +3,22 @@ import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 
 type PaginationProps = {
   totalPages: number;
-  acutalPage: number;
+  actualPage: number;
   handlePageChange: (page: number) => void;
 };
 
-const Pagination = ({ totalPages, acutalPage, handlePageChange }: PaginationProps) => {
+const Pagination = ({ totalPages, actualPage, handlePageChange }: PaginationProps) => {
   return <div className="flex justify-center gap-4">
-    <PaginationButton content={MdKeyboardArrowLeft} handleClick={() => console.log('ae')} actualPage={false}/>
+    <PaginationButton content={MdKeyboardArrowLeft} handleClick={() => handlePageChange(Math.max(1, actualPage - 1))} actualPage={false}/>
     {Array.from({ length: totalPages }, (_, index) => index + 1).map((page) => (
         <PaginationButton 
           key={page} 
           content={page.toString()} 
           handleClick={() => handlePageChange(page)} 
-          actualPage={page.toString() === acutalPage.toString()}
+          actualPage={page.toString() === actualPage.toString()}
         />
       ))}
-    <PaginationButton content={MdKeyboardArrowRight} handleClick={() => console.log('ae')} actualPage={false}/>
+    <PaginationButton content={MdKeyboardArrowRight} handleClick={() => handlePageChange(Math.min(totalPages, actualPage + 1))} actualPage={false}/>
   </div>
 };
 
