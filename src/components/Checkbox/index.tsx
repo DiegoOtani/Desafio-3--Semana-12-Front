ec0ead6c9389139c1f4b1cc8015ea3b8ba1ca9a2
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-08-27 23:06:35 -0300

    style: change font color

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,14 +1,14 @@
 import { CheckboxProps } from './types';
 
 const Checkbox = ({ value, handleChange }: CheckboxProps) => {
-  return <label className='text-gray-400 flex gap-2 text-h6 items-center'>
+  return <label className='text-bodyColor flex gap-2 text-h6 items-center'>
     <input 
       className='hidden peer'
       type="checkbox" 
       value={value}
       onChange={handleChange}
     />
-    <div className='w-4 h-4 border-2 border-gray-400 rounded peer-checked:bg-brand_2 peer-focus:ring'></div>
+    <div className='w-4 h-4 border-2 border-bodyColor rounded peer-checked:bg-brand_2 peer-focus:ring'></div>
     {value}
   </label>
 }
