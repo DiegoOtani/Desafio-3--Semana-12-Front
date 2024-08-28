34d87e40720ad75e2d8db190b37177cffc543d25
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-08-28 15:30:06 -0300

    feat: change requisition to implements page and limit / create states to pagination work

--- src/pages/Tour/index.tsx
+++ src/pages/Tour/index.tsx
@@ -4,34 +4,49 @@ import { useState, useEffect } from "react";
 import { TourReturned } from "../../interfaces/Tour";
 import TourService from "../../services/api/toursService";
 import TourCard from "../../components/TourCard";
+import Pagination from "./Components/Pagination";
 
 const Tour = () => {
   const [tours, setTours] = useState<TourReturned[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [totalPages, setTotalPages] = useState<number>(0);
+  const [totalTours, setTotalTours] = useState<number>(0);
+  const limit = 9;
 
   useEffect(() => {
     const loadTours = async () => {
       try {
-        const data = await TourService.getTours();
-        setTours(data);
+        console.log(currentPage)
+        const data = await TourService.getToursByPage(currentPage, limit);
+        setTours(data.tours);
+        setTotalPages(data.totalPages);
+        setTotalTours(data.total);
       } catch (error) {
         console.log(error);
       }
     };
     loadTours();
-  }, []);
+  }, [currentPage]);
 
-  return (
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
+  return (  
     <>
       <NavSection previousPages={[{ name: 'Home', to: '/' }]} actualPage="Tour Package" />
       <main className="bg-white flex gap-6 p-32">
         <Filters />
-        <section className="grid grid-cols-3 min-w-[75%]">
-          {tours.map(tour => (
-            <TourCard 
-              key={tour.tour_id} 
-              {...tour}
-            />
-          ))}
+        <section className="min-w-[75%] flex flex-col ">
+          <div className="grid grid-cols-3 min-w-[75%]">
+            {tours.map(tour => (
+              <TourCard 
+                key={tour.tour_id} 
+                {...tour}
+              />
+            ))}
+          </div>
+          <Pagination actualPage={currentPage} totalPages={totalPages} handlePageChange={handlePageChange}/>
         </section>
       </main>
     </>
