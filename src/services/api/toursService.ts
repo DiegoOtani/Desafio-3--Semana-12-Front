5b294df0fee9584c56ebb1252b888f9f9ab9df85
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-08-28 15:29:09 -0300

    feat: create service search tours by page and limit

--- src/services/api/toursService.ts
+++ src/services/api/toursService.ts
@@ -16,6 +16,26 @@ class TourService {
       throw error;
     }
   };
+
+  static async getToursByPage(page: number, limit: number) {
+    try {
+      const response = await api.get('/tour/page', {
+        params: {
+          page,
+          limit
+        }
+      });
+      console.log(response.data)
+      return response.data;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.error('Error fetching types:', error.message);
+      } else {
+        console.error('Unexpected error:', error);
+      }
+      throw error;
+    }
+  }
 };
 
 export default TourService;
