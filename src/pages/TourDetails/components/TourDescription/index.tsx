09edaea584d2aaccb4d015d77faa3fa89f063f15
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-08-29 18:30:17 -0300

    style: change litle adjusts in padding and width

--- src/pages/TourDetails/components/TourDescription/index.tsx
+++ src/pages/TourDetails/components/TourDescription/index.tsx
@@ -1,11 +1,11 @@
 import { TourDescriptionProps } from "./types";
 
 const TourDescription = ({ title, info, color = 'black' }: TourDescriptionProps) => {
-  return <div className="w-full flex flex-col text-subtitle font-body">
+  return <div className="flex flex-col text-h6 font-body">
     <h4 className="text-bodyColor">{title}</h4>
     <p className={`
-      font-bold 
-      ${color === 'red' ? 'text-brand_2' : 'text-black'}
+      font-extrabold 
+      ${color === 'red' ? 'text-brand_2' : 'text-secondary'}
     `}>
       {info}
     </p>
