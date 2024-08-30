bc8991eb6ec3ee6ec9d2992e9a06dd03c843ae5d
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-08-30 12:35:29 -0300

    style: add padding y in Stars component

--- src/components/Stars/index.tsx
+++ src/components/Stars/index.tsx
@@ -5,7 +5,7 @@ type StarProp = {
 }
 
 const Stars = ({ value }: StarProp) => {
-  return <div className='flex items-center justify-center bg-brand_2 w-12 rounded'>
+  return <div className='flex items-center justify-center bg-brand_2 w-12 rounded py-1'>
     <IoIosStar size={14} color='white'/>
     <span className='text-white pl-1 text-body font-light'>{value}</span>
   </div>
