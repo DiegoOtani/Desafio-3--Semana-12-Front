95c5a1d13a47537fc8d03d77a4425fe4670a315d
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-08-30 20:30:40 -0300

    feat: add reviews by tour id service and interface

--- src/interfaces/review.ts
+++ src/interfaces/review.ts
@@ -7,3 +7,11 @@ export interface ReviewReturned {
   avg_room_comfort_quality: number;
   avg_overall: number;
 };
+
+export interface ReviewUserReturnded {
+  date_review: string;
+  name: string;
+  average_rating: number;
+  comment: string;
+  review_count_by_user: number;
+};
--- src/services/api/reviewService.ts
+++ src/services/api/reviewService.ts
@@ -1,13 +1,12 @@
 import axios from "axios";
 import { api } from "./api";
-import { ReviewReturned } from "../../interfaces/review";
+import { ReviewReturned, ReviewUserReturnded } from "../../interfaces/review";
 
 class ReviewService {
   static async getReviewAverageByTourId (tourId: string | undefined) {
     try {
       if(tourId === undefined) return;
-      const response = await api.get<{review: ReviewReturned}>(`/review/avg/${tourId}`)
-      console.log(response.data);
+      const response = await api.get<{review: ReviewReturned}>(`/review/avg/${tourId}`);
       return response.data.review;
     } catch (error) {
       if (axios.isAxiosError(error)) {
@@ -17,6 +16,21 @@ class ReviewService {
       }
       throw error;
     };
+  };
+
+  static async getReviewsById(tourId: string | undefined) {
+    try {
+      if(tourId === undefined) return;
+      const response = await api.get<{reviews: ReviewUserReturnded[]}>(`/review/tour/${tourId}`);
+      return response.data.reviews;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.error('Error fetching types:', error.message);
+      } else {
+        console.error('Unexpected error:', error);
+      }
+      throw error;
+    }
   };
 };
 
