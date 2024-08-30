4652ca73a87eeb7caad1f771618088c1254290cf
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-08-30 13:54:28 -0300

    style: change paddin button

--- src/components/SubmitButton/index.tsx
+++ src/components/SubmitButton/index.tsx
@@ -3,7 +3,7 @@ import { SubmitButtonProps } from './types';
 const SubmitButton = ({ onClick, text, size = 'large' }: SubmitButtonProps) => {
   return <button 
       type='button'
-      className={`bg-brand_2 text-white font-normal rounded-lg p-2 hover:bg-red-500 text-h6
+      className={`bg-brand_2 text-white font-normal rounded-lg px-2 py-3 hover:bg-red-500 text-subtitle font-body
         ${size === 'large'
           ? 'w-full'
           : 'w-[45%]'
