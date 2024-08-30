381660c96261b921edfa825e1bb5a392b5265e0d
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-08-30 10:54:43 -0300

    feat: add ReviewSection in TourDetails page

--- src/pages/TourDetails/index.tsx
+++ src/pages/TourDetails/index.tsx
@@ -3,15 +3,18 @@ import ImageSection from "./sections/ImageSection";
 import InfoSection from "./sections/InfoSection";
 import OverviewSection from "./sections/OverviewSection";
 import MapSection from "./sections/MapSection";
+import ReviewSection from "./sections/ReviewsSection";
 
 const TourDetails = () => {
   return <main className="w-full flex justify-center gap-6 px-32 py-10">
-    <div className="w-full flex flex-col gap-6">
+    <div className="w-full flex flex-col gap-10">
       <ImageSection imgUrl="/background-baloon.jpg"/>
-      <h1 className="text-h3 font-extrabold text-secondary font-title">Wonders of the West Coast & Kimberly</h1>
+      <h1 className="text-h4 font-extrabold text-secondary font-title">Wonders of the West Coast & Kimberly</h1>
       <InfoSection min_price="104" duration="7" max_people="25" min_age="12" types="Adventure, Beaches" average="4.8" count_review="15"/>
       <OverviewSection />
       <MapSection coordinates={[-22.9068, -43.1729]} city="Rio de Janeiro" country="Brasil"/>
+      <h1 className="text-h5 font-extrabold text-secondary font-title">Average Reviews</h1>
+      <ReviewSection />
     </div>
     <AsidePurchase min_price="104"/> 
   </main>
