ac6f675244324516d1c40eda00bffb4f5b1b1cc3
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-08-29 21:19:51 -0300

    feat: initial structure on TourDetails page

--- src/pages/TourDetails/index.tsx
+++ src/pages/TourDetails/index.tsx
@@ -1,7 +1,18 @@
+import AsidePurchase from "./sections/AsidePurchase";
+import ImageSection from "./sections/ImageSection";
+import InfoSection from "./sections/InfoSection";
+import OverviewSection from "./sections/OverviewSection";
+
 const TourDetails = () => {
-  return <div>
-  
-  </div>
+  return <main className="w-full flex justify-center gap-6 px-32 py-10">
+    <div className="w-full flex flex-col">
+      <ImageSection imgUrl="/background-baloon.jpg"/>
+      <h1 className="text-h3 font-extrabold text-secondary font-title">Wonders of the West Coast & Kimberly</h1>
+      <InfoSection min_price="104" duration="7" max_people="25" min_age="12" types="Adventure, Beaches" average="4.8" count_review="15"/>
+      <OverviewSection />
+    </div>
+    <AsidePurchase min_price="104"/> 
+  </main>
 };
 
 export default TourDetails;
