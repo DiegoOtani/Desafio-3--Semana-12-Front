9e1c52201470465e013b76b83ea0faef5b7e71de
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-09-01 15:37:33 -0300

    fix: toFixed in average review into TourCard

--- src/components/Stars/index.tsx
+++ src/components/Stars/index.tsx
@@ -7,7 +7,7 @@ type StarProp = {
 const Stars = ({ value }: StarProp) => {
   return <div className='flex items-center justify-center bg-brand_2 w-12 rounded py-1'>
     <IoIosStar size={14} color='white'/>
-    <span className='text-white pl-1 text-body font-light'>{value}</span>
+    <span className='text-white pl-1 text-body font-light'>{Number(value).toFixed(1)}</span>
   </div>
 }
 
