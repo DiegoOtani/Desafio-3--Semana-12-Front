c19b2859d04364c5404b7e0e1d1dfb5454f9ee8d
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-09-01 10:47:55 -0300

    feat: add userId from firebase, name,email and comment state on inputs

--- src/pages/TourDetails/sections/AddReviewSection/index.tsx
+++ src/pages/TourDetails/sections/AddReviewSection/index.tsx
@@ -1,23 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import StarRating from "../../components/StarRating";
 import SubmitButton from "../../../../components/SubmitButton";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const AddReviewSection = () => {
-
+  const [userId, setUserId] = useState<string | null>("");
+  const [name ,setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [comment, setComment] = useState<string>("");
   const [rating, setRating] = useState<{[key: string]: number}>({
     services: 0,
     locations: 0,
     amentities: 0,
     prices: 0,
     comfort: 0,
-  })
+  });
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if(user) return setUserId(user.uid);
+      setUserId(null);
+    });
+    return () => unsubscribe();
+  }, [userId]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    if (name === "name") setName(value);
+    if (name === "email") setEmail(value);
+    if (name === "comment") setComment(value);
+  };
 
   const handleRatingChange = (key: string) => (rating: number) => {
     setRating((prevRatings) => ({
       ...prevRatings,
       [key]: rating,
     }));
-  }
+  };
 
   return <section className="bg-surface p-10">
     <h2 className="text-secondary font-extrabold text-h5 pb-2">Add a review</h2>
@@ -33,15 +53,24 @@ const AddReviewSection = () => {
         <input className="p-6 w-full focus:outline-none"
           type="text" 
           placeholder="Your name"
+          name="name"
+          value={name}
+          onChange={handleChange}
         />
         <input className="p-6 w-full focus:outline-none"
           type="email" 
           placeholder="Email adress"
+          name="email"
+          value={email}
+          onChange={handleChange}
         />
       </div>
       <input className="p-6 pb-20 w-full focus:outline-none"
         type="text" 
         placeholder="Write your comment"
+        name="comment"
+        value={comment}
+        onChange={handleChange}
       />
       <SubmitButton size="very small" onClick={() => console.log(rating.prices)} text="Submit review"/>
     </form>
