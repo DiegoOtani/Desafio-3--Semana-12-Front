fd92c64b84ecbbd7615e7e42a92846eafdb4f3b8
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-09-01 11:22:13 -0300

    feat: add postReview service

--- src/services/api/reviewService.ts
+++ src/services/api/reviewService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { api } from "./api";
-import { ReviewReturned, ReviewUserReturnded } from "../../interfaces/review";
+import { ReviewReturned, ReviewUserReturnded, PostReview, CompleteReview } from "../../interfaces/review";
 
 class ReviewService {
   static async getReviewAverageByTourId (tourId: string | undefined) {
@@ -23,6 +23,20 @@ class ReviewService {
       if(tourId === undefined) return;
       const response = await api.get<{reviews: ReviewUserReturnded[]}>(`/review/tour/${tourId}`);
       return response.data.reviews;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.error('Error fetching types:', error.message);
+      } else {
+        console.error('Unexpected error:', error);
+      }
+      throw error;
+    }
+  };
+
+  static async postReview(reviewData: PostReview) {
+    try {
+      const response = await api.post<{ review: CompleteReview, message:string }>(`/review`, reviewData);
+      return response.data;
     } catch (error) {
       if (axios.isAxiosError(error)) {
         console.error('Error fetching types:', error.message);
