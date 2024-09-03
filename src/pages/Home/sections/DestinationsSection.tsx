af7756c1976c62239e856fd4994ab78012ec8bd6
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-09-03 17:22:18 -0300

    style: changing destinations images

--- src/pages/Home/sections/DestinationsSection.tsx
+++ src/pages/Home/sections/DestinationsSection.tsx
@@ -1,20 +1,53 @@
 import Titles from "../../../components/Titles";
 import Destinations from "../components/Destinations";
-import backgroundImage from '/woman-taking-photo-camera-while-her-friends-looking-map.jpg'
-import countryImage from '/london-eye-e-uma-enorme.jpg'
 
 const DestinationsSection = () => {
   return <section className="bg-surface flex flex-col items-center gap-10 w-full px-20 py-32">
     <Titles colorTitle="black" size="medium" subtitle="Destination" title="Top Attractions Destinations"/>
     <div className="w-full h-[500px] flex justify-center items-center">
       <div className="w-full max-w-[70%] h-full flex flex-wrap justify-center gap-4">
-        <Destinations backgroundImage={countryImage} travelers="174,688" country="United Kingdom" width={30} height="medium"/>
-        <Destinations backgroundImage={backgroundImage} travelers="174,688" country="Turkey" width={30} height="medium"/>
-        <Destinations backgroundImage={backgroundImage} travelers="174,688" country="Turkey" width={30} height="medium"/>
-        <Destinations backgroundImage={backgroundImage} travelers="174,688" country="Turkey" width={45} height="medium"/>
-        <Destinations backgroundImage={backgroundImage} travelers="174,688" country="Turkey" width={45} height="medium"/>
+        <Destinations backgroundImage={'https://firebasestorage.googleapis.com/v0/b/trisog-travels.appspot.com/o/bg.jpeg?alt=media&token=efb10064-49be-414c-95d2-a0d4c9ee6589'} 
+          travelers="174,688" 
+          country="United Kingdom" 
+          width={30} 
+          height="medium"
+        />
+        <Destinations 
+          backgroundImage={'https://firebasestorage.googleapis.com/v0/b/trisog-travels.appspot.com/o/bg.jpeg?alt=media&token=efb10064-49be-414c-95d2-a0d4c9ee6589'} 
+          travelers="174,688" 
+          country="Turkey" 
+          width={30} 
+          height="medium"
+        />
+        <Destinations 
+          backgroundImage={'https://firebasestorage.googleapis.com/v0/b/trisog-travels.appspot.com/o/bg.jpeg?alt=media&token=efb10064-49be-414c-95d2-a0d4c9ee6589'} 
+          travelers="174,688" 
+          country="Turkey" 
+          width={30} 
+          height="medium"
+        />
+        <Destinations 
+        backgroundImage={'https://firebasestorage.googleapis.com/v0/b/trisog-travels.appspot.com/o/australia.jpg?alt=media&token=5982bbf2-0ac5-4103-9efe-e8a4a1fdfeb3'} 
+          travelers="174,688" 
+          country="Turkey" 
+          width={45} 
+          height="medium"
+        />
+        <Destinations 
+          backgroundImage={'https://firebasestorage.googleapis.com/v0/b/trisog-travels.appspot.com/o/australia.jpg?alt=media&token=5982bbf2-0ac5-4103-9efe-e8a4a1fdfeb3'} 
+          travelers="174,688" 
+          country="Turkey" 
+          width={45} 
+          height="medium"
+        />
       </div>
-      <Destinations backgroundImage={backgroundImage} travelers="174,688" country="Turkey" width={20} height="full"/>
+      <Destinations 
+        backgroundImage={'https://firebasestorage.googleapis.com/v0/b/trisog-travels.appspot.com/o/francesco-la-corte-LBKny-s6AXg-unsplash.jpg?alt=media&token=4f234689-811e-425f-a34a-8db4b88a1dbb'} 
+        travelers="174,688" 
+        country="Turkey" 
+        width={20} 
+        height="full"
+      />
     </div>
   </section>
 }
